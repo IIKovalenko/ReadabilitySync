364fe116303e9fe02c9c987faab97d86570aedaa
Melevir <melevir@gmail.com>
2013-08-20 07:10:18 +0400

    More wireframe added

--- sync.py
+++ sync.py
@@ -32,7 +32,7 @@ def get_readability_bookmarks(rdd):
 
 def get_sync_dir():
     """ Return dir that should be synced with readability account"""
-    return os.environ.get('READABILITY_SYNC_DIR',  '~/sync_test')
+    return os.environ.get('READABILITY_SYNC_DIR',  '~/sync_test')  # TODO Get from user params
 
 def validate_dir(dir, validate_existance=True, validate_read_rights=True, vallidat_write_rights=True):
     """ Raisies error if dir doesn't exist or now rw rights"""
@@ -42,7 +42,15 @@ def get_books_from_dir(dir):
     """ Returns info about books currently in dir"""
     books = [f for f in listdir(dir) if isfile(join(dir,f)) and f.split('.')[-1] == 'epub']
     books_info = [get_epub_info(b) for b in books]
+    for book in books_info:
+        finished = get_finished_in_percents(book)
+        if finished:
+            book['percents_finished'] = finsihed
     return books_info
+
+def get_finished_in_percents(book_info):
+    """ Retrieve info about how many of book already been read (if possible)"""
+    return 0  # TODO
 
 def delete_books_not_in_bookmarks(dir, books, bookmarks):
     """ Deletes books in folder, but not in readability"""
@@ -50,7 +58,12 @@ def delete_books_not_in_bookmarks(dir, books, bookmarks):
         if not book['title'] in [a['title'] for a in bookmarks]:
             pass  # TODO delete book
 
-def download_new_books(dir, books, bookmarks):
+def delete_finished_books(dir, books):
+    for book in books:
+        if book.get('percents_finished', '') == 100:
+            pass  # TODO delete_book
+
+def download_new_books(rdd, dir, books, bookmarks):
     """ Download books in readability, but not in folder"""
     for bookmark in bookmarks:
         if not bookmark['title'] in [b['title'] for b in books]:
@@ -62,4 +75,4 @@ sync_dir = get_sync_dir()
 validate_dir(sync_dir)
 books = get_books_from_dir(sync_dir)
 delete_books_not_in_bookmarks(sync_dir, books, bookmarks)
-download_new_books(sync_dir, books, bookmarks)
+download_new_books(rdd, sync_dir, books, bookmarks)
